c4db1cc4f933c36d9442bce67a2fd431c7d4be47
ThatTobMate <thale.android@gmail.com>
2015-01-25 18:43:22 +0000

    routes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,5 @@
+/* global app:true */
+/* exported app */
 'use strict';
 
 /**
@@ -8,7 +10,7 @@
  *
  * Main module of the application.
  */
-angular
+app = angular
   .module('clutchFirebaseApp', [
     'ngAnimate',
     'ngCookies',
@@ -20,12 +22,64 @@ angular
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
+        templateUrl: 'views/home.html',
         controller: 'MainCtrl'
       })
       .when('/about', {
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
+      })
+      .when('/profile/:userId', {
+        templateUrl: 'views/profile.html',
+        controller: 'MainCtrl'
+      })
+      .when('/consoles', {
+        templateUrl: 'views/consoles.html',
+        controller: 'MainCtrl'
+      })
+      .when('/consoles/:consoleName', {
+        templateUrl: 'views/console_games.html',
+        controller: 'MainCtrl'
+      })
+      .when('/consoles/:consoleName/:gameTitle', {
+        templateUrl: 'views/games_ladders.html',
+        controller: 'MainCtrl'
+      })
+      .when('/consoles/:consoleName/:gameTitle/:ladderId', {
+        templateUrl: 'views/ladder.html',
+        controller: 'MainCtrl'
+      })
+      .when('/consoles/:consoleName/:gameTitle/:ladderId/create', {
+        templateUrl: 'views/create_team.html',
+        controller: 'MainCtrl'
+      })
+      .when('/consoles/:consoleName/:gameTitle/:ladderId/rules', {
+        templateUrl: 'views/rules.html',
+        controller: 'MainCtrl'
+      })
+      .when('/consoles/:consoleName/:gameTitle/:ladderId/:teamId', {
+        templateUrl: 'views/team.html',
+        controller: 'MainCtrl'
+      })
+      .when('/consoles/:consoleName/:gameTitle/:ladderId/:teamId/results', {
+        templateUrl: 'views/results.html',
+        controller: 'MainCtrl'
+      })
+      .when('/consoles/:consoleName/:gameTitle/:ladderId/:teamId/results/:matchId', {
+        templateUrl: 'views/match_details.html',
+        controller: 'MainCtrl'
+      })
+      .when('/consoles/:consoleName/:gameTitle/:ladderId/matches', {
+        templateUrl: 'views/matches.html',
+        controller: 'MainCtrl'
+      })
+      .when('/consoles/:consoleName/:gameTitle/:ladderId/matches/create', {
+        templateUrl: 'views/create_match.html',
+        controller: 'MainCtrl'
+      })
+      .when('/consoles/:consoleName/:gameTitle/:ladderId/matches/:matchId', {
+        templateUrl: 'views/match_details.html',
+        controller: 'MainCtrl'
       })
       .otherwise({
         redirectTo: '/'
